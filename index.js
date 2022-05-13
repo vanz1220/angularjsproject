388604e1695249928b443f36cea3cc1b796e6ffc
vanz1220 <ije.malong@gmail.com>
2022-05-13 16:52:08 +0800

    angularjs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaces&date=2022-05-13").then(function (response) {
-      $scope.myData = response.data.races;
+app.controller('raceCtrl', function($scope, $http) {
+  $http.get("https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaceRunners&marketId=1.199021059").then(function (response) {
+      $scope.myData = response.data.runners;
   });
 });
