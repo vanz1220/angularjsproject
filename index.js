a1f3033b8c88dfa8b50ac4690fc6544e86b6b4a7
Ivan Jan Malong <ije.malong@gmail.com>
2022-05-17 11:19:49 +0800

    angularjs

--- index.js
+++ index.js
@@ -24,27 +24,14 @@ angular.module('app', [])
             //console.log("DATE: ",$scope.finaldate);
 
             $http.get("https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaces&date="+ $scope.finaldate).then(function (response) {
-                
                 $scope.myData1 = response.data.races;
                 //console.log("Races: ",$scope.myData1);
-                if($scope.myData1 != null){
-                angular.forEach( $scope.myData1, function(b4y_datetime) {
-                    //console.log("DATETIME: ", b4y_datetime.date_time);
-                  });
-                  angular.forEach( $scope.myData1, function(b4y_venue) {
-                    //console.log("VENUE: ", b4y_venue.venue);
-                  });
-                  angular.forEach( $scope.myData1, function(b4y_marketid) {
-                    //console.log("MARKETID: ", b4y_marketid.marketId);
-                  });
-                }
             })
         }
 
          $scope.getdetails = function () {
           $scope.xmarketid = $scope.x.marketId;
           
-
           $http.get('https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaceRunners&marketId='+ $scope.xmarketid).then(function(response1){
                 $scope.runnersList = response1.data.runners;
                 //console.log("Runners: ",$scope.runnersList);
