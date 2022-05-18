94e687570bd26d0de7b2fbe6f27db1a41b954b50
Ivan Jan Malong <ije.malong@gmail.com>
2022-05-18 11:37:05 +0800

    angularjs

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
@@ -18,23 +16,38 @@ angular.module('app', [])
     .controller('ctrl', function($scope, $http){
       
         $scope.myvalue = false;
+        $scope.myvalue2 = false;
 
         $scope.getdatedetails = function () {
             $scope.myvalue = false;
+            $scope.myvalue2 = false;
+
             $scope.finaldate = formatDate($scope.datedetails);
             //console.log("DATE: ",$scope.finaldate);
 
-            $http.get("https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaces&date="+ $scope.finaldate).then(function (response) {
-                $scope.myData1 = response.data.races;
-                //console.log("Races: ",$scope.myData1);
+            $http.get("https://bet4you.co.uk/wp-content/plugins/b4y_ratings/api/models/DisplayRating.php?day="+ $scope.finaldate).then(function (response) {
+                    
+                    if(!Object.keys(response.data.races).length){
+                        $scope.msg = "NO DATA AT THE MOMENT!!!";
+                        //console.log("NO DATA AT THE MOMENT");
+                        $scope.myvalue2 = true;
+                        $scope.myvalue = false;
+                        $scope.myData1 = [];
+                    }else{
+                        $scope.myData1 = response.data.races;
+                        //console.log("Races: ",$scope.myData1);
+                        $scope.myvalue2 = false;
+                        $scope.myvalue = true;
+                        $scope.msg = "";
+                    }
             })
         }
 
          $scope.getdetails = function () {
                 $scope.xmarketid = $scope.x.marketId;
-                $http.get('https://hedgerpro.co.uk/api/rpc/remote/b4y.php?action=getRaceRunners&marketId='+ $scope.xmarketid).then(function(response1){
-                $scope.runnersList = response1.data.runners;
-                //console.log("Runners: ",$scope.runnersList);
+                $http.get('https://bet4you.co.uk/wp-content/plugins/b4y_ratings/api/models/DisplayRunners.php?marketid='+ $scope.xmarketid).then(function(response1){
+                $scope.runnersList = response1.data.ratings;
+                console.log("Runners: ",$scope.runnersList);
                 })
         }
     })
